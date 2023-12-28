311a9811b49223c3555769f427e349f9abdcab26
dynadude <sagi.gam.11@gmail.com>
2023-12-28 11:54:12 +0000

    completely revamped src/test/java/io/jenkins/plugins/scmfilter/impl/trait/WildcardSCMPrFilterTraitTest.java and added a single test

--- src/test/java/io/jenkins/plugins/scmfilter/impl/trait/WildcardSCMPrFilterTraitTest.java
+++ src/test/java/io/jenkins/plugins/scmfilter/impl/trait/WildcardSCMPrFilterTraitTest.java
@@ -1,75 +1,67 @@
 package io.jenkins.plugins.scmfilter.impl.trait;
 
-import hudson.model.FreeStyleBuild;
-import hudson.model.FreeStyleProject;
-import hudson.model.Label;
-import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
-import org.jenkinsci.plugins.workflow.job.WorkflowJob;
-import org.jenkinsci.plugins.workflow.job.WorkflowRun;
-import org.junit.Rule;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import hudson.model.TaskListener;
+import java.io.IOException;
+import java.util.List;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.SCMHeadObserver;
+import jenkins.scm.api.SCMSourceCriteria;
+import jenkins.scm.api.trait.SCMHeadPrefilter;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.impl.mock.MockSCMController;
+import jenkins.scm.impl.mock.MockSCMSource;
+import jenkins.scm.impl.mock.MockSCMSourceContext;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
-public class AnySCMFIlterTraitTest {
-
-    @Rule
-    public JenkinsRule jenkins = new JenkinsRule();
-
-    final String name = "Bobby";
+public class WildcardSCMPrFilterTraitTest {
+    private String mockRepoName = "mock-repo";
+    private String[] mockBranches = {"master", "develop", "staging"};
 
     @Test
-    public void testConfigRoundtrip() throws Exception {
-        FreeStyleProject project = jenkins.createFreeStyleProject();
-        project.getBuildersList().add(new HelloWorldBuilder(name));
-        project = jenkins.configRoundtrip(project);
-        jenkins.assertEqualDataBoundBeans(
-                new HelloWorldBuilder(name), project.getBuildersList().get(0));
+    public void testPrToMasterNotFiltered() throws Exception {
+        SCMSourceContext context = initializeMockSCMSourceContext();
+        WildcardSCMPrFilterTrait prWildcardFilter = new WildcardSCMPrFilterTrait(mockBranches[0], "fgd", "", "*");
+        prWildcardFilter.decorateContext(context);
+        List<SCMHeadPrefilter> prefilters = context.prefilters();
+        for (SCMHeadPrefilter prefilter : prefilters) {
+            assertTrue(
+                    "Is the PR let through?", prefilter.isExcluded(initializeMockSCMSource(), new SCMHead("test-pr") {
+                        public SCMHead getTarget() {
+                            return new SCMHead(mockBranches[0]);
+                        }
+                    }));
+        }
     }
 
-    @Test
-    public void testConfigRoundtripFrench() throws Exception {
-        FreeStyleProject project = jenkins.createFreeStyleProject();
-        HelloWorldBuilder builder = new HelloWorldBuilder(name);
-        builder.setUseFrench(true);
-        project.getBuildersList().add(builder);
-        project = jenkins.configRoundtrip(project);
-
-        HelloWorldBuilder lhs = new HelloWorldBuilder(name);
-        lhs.setUseFrench(true);
-        jenkins.assertEqualDataBoundBeans(lhs, project.getBuildersList().get(0));
+    private MockSCMSourceContext initializeMockSCMSourceContext() throws IOException {
+        SCMHeadObserver observer = mock(SCMHeadObserver.class);
+        return new MockSCMSourceContext(
+                initializeMockSCMSource(),
+                new SCMSourceCriteria() {
+                    public boolean isHead(Probe prb, TaskListener lsnr) {
+                        return true;
+                    }
+                },
+                observer);
     }
 
-    @Test
-    public void testBuild() throws Exception {
-        FreeStyleProject project = jenkins.createFreeStyleProject();
-        HelloWorldBuilder builder = new HelloWorldBuilder(name);
-        project.getBuildersList().add(builder);
-
-        FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
-        jenkins.assertLogContains("Hello, " + name, build);
+    private MockSCMSource initializeMockSCMSource() throws IOException {
+        return new MockSCMSource(initializeMockSCMController(), mockRepoName);
     }
 
-    @Test
-    public void testBuildFrench() throws Exception {
+    private MockSCMController initializeMockSCMController() throws IOException {
+        MockSCMController controller = MockSCMController.create();
 
-        FreeStyleProject project = jenkins.createFreeStyleProject();
-        HelloWorldBuilder builder = new HelloWorldBuilder(name);
-        builder.setUseFrench(true);
-        project.getBuildersList().add(builder);
+        controller.createRepository(mockRepoName);
+        for (String branch : mockBranches) {
+            controller.createBranch(mockRepoName, branch);
+        }
 
-        FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
-        jenkins.assertLogContains("Bonjour, " + name, build);
-    }
+        controller.openChangeRequest(mockRepoName, mockBranches[0]);
 
-    @Test
-    public void testScriptedPipeline() throws Exception {
-        String agentLabel = "my-agent";
-        jenkins.createOnlineSlave(Label.get(agentLabel));
-        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
-        String pipelineScript = "node {greet '" + name + "'}";
-        job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
-        WorkflowRun completedBuild = jenkins.assertBuildStatusSuccess(job.scheduleBuild2(0));
-        String expectedString = "Hello, " + name + "!";
-        jenkins.assertLogContains(expectedString, completedBuild);
+        return controller;
     }
 }
