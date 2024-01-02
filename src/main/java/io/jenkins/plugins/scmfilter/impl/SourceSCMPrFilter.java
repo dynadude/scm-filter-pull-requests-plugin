9482e60c4fa04472ecd6edbcced50f67c0dd5860
Sagi Gamliel <sagi.gam.11@gmail.com>
2024-01-02 21:33:53 +0000

    added src/main/java/io/jenkins/plugins/scmfilter/impl/SourceSCMPrFilter.java

---
+++ src/main/java/io/jenkins/plugins/scmfilter/impl/SourceSCMPrFilter.java
@@ -0,0 +1,24 @@
+package io.jenkins.plugins.scmfilter.impl;
+
+import io.jenkins.plugins.scmfilter.SCMHeadMatcher;
+import io.jenkins.plugins.scmfilter.SCMPrFilter;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.mixin.ChangeRequestSCMHead2;
+
+public class SourceSCMPrFilter implements SCMPrFilter {
+    private SCMHeadMatcher matcher;
+
+    public SourceSCMPrFilter(SCMHeadMatcher matcher) {
+        this.matcher = matcher;
+    }
+
+    public boolean isExcluded(SCMHead head) {
+        if (!(head instanceof ChangeRequestSCMHead2)) {
+            return false;
+        }
+
+        head = new SCMHead(((ChangeRequestSCMHead2) head).getOriginName());
+
+        return !matcher.doesMatch(head);
+    }
+}
